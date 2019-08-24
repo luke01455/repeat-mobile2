42846f67e0d44d00820a4af566b09afffd52ca1b
luke01455 <luke0145555@hotmail.co.uk>
2019-08-24 20:35:23 +0100

    mid section styling more or less complete

--- src/App.js
+++ src/App.js
@@ -22,14 +22,20 @@ function App() {
         </div>
       </div>
       <div className="App-header">
+        <div className="SaveText-Container">
         <p id="MidText-1"> Save the last... </p>
         <p id="MidText-2"> 10 Minutes </p>
-        <div> 
+        </div>
+        <div className="Rangebar-Container"> 
           <input type="range" class="range" name="" defaultValue="10"
           min="0" max="20"></input>
         </div>
+        <div className="cd-container">
         <img src={cddvd} className="App-logo" alt="logo" />
+        </div>
+        <div className= "Savebtn-Container">
         <button className="Signup-button" id="Savebtn">Save</button>
+        </div>
       </div>
       <div className="Bottom-section">
         <div className="buttons-container">
