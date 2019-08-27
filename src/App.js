a1d98d54b00ab38be2163e177b2d025b17943d02
luke01455 <luke0145555@hotmail.co.uk>
2019-08-27 13:23:47 +0100

    started adding base login forms

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import HomePage from './pages/homepage.component';
-
+import HomePage from './pages/homepage/homepage.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div className="App">
       <Switch>
       <Route exact path='/' component={HomePage} />
+      <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
